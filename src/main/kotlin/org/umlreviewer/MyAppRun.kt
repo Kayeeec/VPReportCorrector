3b4021d0e7e1b69af95ab7fb96bcade719f9c25f
Karolína Božková <karolina.bozkova@gmail.com>
2021-05-16 23:14:32 +0200

    fat jar

--- src/main/kotlin/org/umlreviewer/MyAppRun.kt
+++ src/main/kotlin/org/umlreviewer/MyAppRun.kt
@@ -1,10 +1,5 @@
 package org.umlreviewer
 
-class MyAppRun {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            MyApp().main(arrayOf())
-        }
-    }
+fun main(args: Array<String>) {
+    MyApp().main(args)
 }
